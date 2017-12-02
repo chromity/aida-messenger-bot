55260620a5e671cabda5151a0a137ea92d53d012
Michael Daryl Mayo <michaeldaryl.code@gmail.com>
2017-12-03 01:42:06 +0800

    Add health condition, test some things

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -38,12 +38,14 @@ def get_education
           text: "Enter your highest education attainment",
           buttons: [
             { type: 'postback', title: "Elementary", payload: "Elementary" },
-            { type: 'postback', title: "High School", payload: "High School"},
+            { type: 'postback', title: "Senior High School", payload: "Senior High School"},
             { type: 'postback', title: "College", payload: "College"}
           ]
         }
       }
     )
+
+    get_education_postback()
   end
 end
 
@@ -56,7 +58,30 @@ def get_education_postback
 end
 
 def get_health_condition
+  Bot.on :message do |message|
+    message.reply(
+      attachment: {
+        type: 'template',
+        payload: {
+          template_type: 'button', # change this
+          text: "Enter your highest education attainment",
+          buttons: [
+            { type: 'postback', title: "Poor", payload: "Poor" },
+            { type: 'postback', title: "Fair", payload: "Fair"},
+            { type: 'postback', title: "Good", payload: "Good"}
+          ]
+        }
+      }
+    )
+  end
+
+  get_health_postback()
 end
 
+def get_health_postback
+  Bot.on :postback do |postback|
+    message.reply(text: "HEH")
+  end
+end
 
 welcome
