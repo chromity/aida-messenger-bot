b840ff631d05d98141af6fc35e71a10232ad7998
Michael Daryl Mayo <michaeldaryl.code@gmail.com>
2017-12-03 01:27:58 +0800

    Test choices

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -24,9 +24,8 @@ def get_full_name
   Bot.on :message do |message|
     name = message.text
     message.reply(text: "Thank you #{name}. Now, please answer some questions to get started. Type y to start.")
+    get_education()
   end
-
-  get_education()
 end
 
 def get_education
@@ -38,46 +37,6 @@ def get_education
            content_type: 'text',
            title: "Primary School",
            payload: "Primary School"
-        },
-        {
-           content_type: 'text',
-           title: "Grade School",
-           payload: "Grade School"
-        },
-        {
-           content_type: 'text',
-           title: "Junior High School",
-           payload: "Junior High School"
-        },
-        {
-           content_type: 'text',
-           title: "Senior High School",
-           payload: "Senior High School"
-        },
-        {
-           content_type: 'text',
-           title: "Vocational Course",
-           payload: "Vocational Course"
-        },
-        {
-           content_type: 'text',
-           title: "Associate's Degree",
-           payload: "Associate's Degree"
-        },
-        {
-           content_type: 'text',
-           title: "Bachelor's Degree",
-           payload: "Bachelor's Degree"
-        },
-        {
-           content_type: 'text',
-           title: "Master's Degree",
-           payload: "Master's Degree"
-        },
-        {
-           content_type: 'text',
-           title: "Doctorate",
-           payload: "Doctorate"
         }
       ])
       get_education_postback()
