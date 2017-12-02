b68eed6fcdae36b7a2d0f95b36d53d1b135b2cf0
Michael Daryl Mayo <michaeldaryl.code@gmail.com>
2017-12-03 00:34:57 +0800

    Add name and education attainment input

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -4,6 +4,46 @@ include Facebook::Messenger
 
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
-Bot.on :message do |message|
-  puts "got your message!"
+Facebook::Messenger::Profile.set({
+  greeting: [
+    {
+      locale: 'default',
+      text: 'Your Smart Investment Messenger Bot.'
+    }
+  ]
+  }, access_token: ENV['ACCESS_TOKEN'])
+
+def welcome
+  Bot.on :message do |message|
+    message.reply(text: 'Hello, to get started please enter your full name.')
+    get_full_name
+  end
 end
+
+def get_full_name
+  Bot.on :message do |message|
+    name = message.text
+    message.reply(text: "Thank you #{name}. Now, please answer some questions to get started. Type y to start.")
+    get_education
+  end
+end
+
+def get_education
+  Bot.on :message do |message|
+    message.reply(
+      text: 'What is your highest educational attainment?',
+      quick_replies: User.educational_attainments.keys.map{ |k|
+        {
+           content_type: 'text',
+           title: k,
+           payload: k
+        }
+      })
+  end
+
+  Bot.on :postback do |postback|
+    message.reply(text: "#{postback.payload}")
+  end
+end
+
+welcome
